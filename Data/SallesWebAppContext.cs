5ffd7ba7ffb4e8521c0875b713420905345b73f6
Pedro Silva <pedro.conceicao2014@gmail.com>
2023-11-07 14:21:09 -0300

    CRUD Scaffolonding

---
+++ Data/SallesWebAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SallesWebApp.Models;
+
+namespace SallesWebApp.Data
+{
+    public class SallesWebAppContext : DbContext
+    {
+        public SallesWebAppContext (DbContextOptions<SallesWebAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SallesWebApp.Models.Departament> Departament { get; set; } = default!;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SallesWebApp.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<SallesWebAppContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SallesWebAppContext") ?? throw new InvalidOperationException("Connection string 'SallesWebAppContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
